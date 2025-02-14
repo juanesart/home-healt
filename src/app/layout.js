0001bbe1837084113054695d7b689f32861f718d
juanesart <juanesartq@gmail.com>
2025-02-14 02:32:12 -0500

    add: gtag-new next version

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,40 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className=" scroll-smooth" lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16821835763"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16821835763');
+            `,
+          }}
+        />
+        {/* Evento de conversión */}
+        <Script
+          id="conversion-event"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              gtag('event', 'conversion', {
+                'send_to': 'AW-16821835763/eDf8CI3snYsaEPOvo9U-',
+                'value': 1.0,
+                'currency': 'COP',
+                'transaction_id': ''
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
